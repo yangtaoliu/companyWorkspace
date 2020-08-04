d8c561d045f37a7f2ae459d4d0a21150086171ee
insist <983107244@qq.com>
2020-08-04 10:02:07 +0800

    修改方法

--- git-test-project/src/cn/study/git/FirstClass.java
+++ git-test-project/src/cn/study/git/FirstClass.java
@@ -8,7 +8,6 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
-        System.out.println("hello git project");
     }
 
     public void test(){
